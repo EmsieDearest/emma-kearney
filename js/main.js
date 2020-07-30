97c6b8c6ccfdbf473300f311d86953204eeaa642
EmsieDearest <ekearney7788@gmail.com>
2020-07-30 17:05:36 -0500

    The whole enchalada

--- js/main.js
+++ js/main.js
@@ -1,29 +1,120 @@
-const copyToClipboard = () => {
-    /* Get the email */
-    let $body = document.getElementsByTagName('body')[0];
-    let $tempInput = document.createElement('INPUT');
-    let $btnCopy = document.getElementById('btnCopy');
-    let secretInfo = 'emma@emma-kearney.com';
-  
-    /* Create a temporary input */
-    $body.appendChild($tempInput);
+// Auto resizes the message input in the contact form
 
-    /* Highlight */
-    $tempInput.setAttribute('value', secretInfo)
-    $tempInput.select();
-  
-    /* Copy */
-    document.execCommand("copy");
+// Targets all textareas with class "message-input"
+let textareas = document.querySelectorAll('.message-input'),
+    hiddenDiv = document.createElement('div'),
+    content = null;
 
-    /* Remove temporary input */
-    $body.removeChild($tempInput);
-  
-    /* Alerting that the email has been copied */
-    document.getElementById("hiddentext").innerHTML = "YOUR WISH IS MY COMMAND!";
-  };
+// Adds a class to all textareas
+for (let j of textareas) {
+    j.classList.add('txtstuff');
+}
 
-const whenBtnIsClicked = () => { 
-    document.getElementById('contact').innerHTML='emma@emma-kearney.com';
-    document.getElementById("hiddentext").innerHTML = "COPY TO CLIPBOARD?";
+// Build the hidden div's attributes
+
+// The line below is needed if you move the style lines to CSS
+// hiddenDiv.classList.add('hiddendiv');
+
+// Add the "message-input" styles, which are common to both textarea and hiddendiv
+// If you want, you can remove those from CSS and add them via JS
+hiddenDiv.classList.add('message-input');
+
+// Add the styles for the hidden div
+// These can be in the CSS, just remove these three lines and uncomment the CSS
+hiddenDiv.style.display = 'none';
+hiddenDiv.style.whiteSpace = 'pre-wrap';
+hiddenDiv.style.wordWrap = 'break-word';
+
+// Loop through all the textareas and add the event listener
+for(let i of textareas) {
+    (function(i) {
+        // Note: Use 'keyup' instead of 'input'
+        // if you want older IE support
+        i.addEventListener('input', function() {
+
+            // Append hiddendiv to parent of textarea, so the size is correct
+            i.parentNode.appendChild(hiddenDiv);
+
+            // Remove this if you want the user to be able to resize it in modern browsers
+            i.style.resize = 'none';
+
+            // This removes scrollbars
+            i.style.overflow = 'hidden';
+
+            // Every input/change, grab the content
+            content = i.value;
+
+            // Add the same content to the hidden div
+
+            // This is for old IE
+            content = content.replace(/\n/g, '<br>');
+
+            // The <br ..> part is for old IE
+            hiddenDiv.innerHTML = content + '<br style="line-height: 3px;">';
+
+            // Briefly make the hidden div block but invisible
+            // This is in order to read the height
+            hiddenDiv.style.visibility = 'hidden';
+            hiddenDiv.style.display = 'block';
+            hiddenDiv.style.minHeight = 400;
+            i.style.minHeight = hiddenDiv.offsetHeight + 'px';
+
+            // Make the hidden div display:none again
+            hiddenDiv.style.visibility = 'visible';
+            hiddenDiv.style.display = 'none';
+        });
+    })(i);
+}
+
+
+
+
+
+
+// Contact form
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+    $(form).submit(function(){
+        $('#my-form-button').hide();
+        $('.md-form').hide();
+        $('.success').show();
+        event.preventDefault(); // if you want to send data only, do not reload page.
+    });
+
+    function error() {
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
 };
-  
